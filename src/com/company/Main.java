efa946744b80d30dd2f4910affc39aae820f69ea
Ivan Segui <ivanseguileyton@gmail.com>
2016-10-14 19:27:43 +0200

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,9 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hola mundo, este es mi primer programa");
+        // write your code here
+    }
+}
